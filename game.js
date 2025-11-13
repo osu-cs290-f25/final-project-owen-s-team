d5296f432e321a43a68a71fd32c6e27051be39ee
Owen Poole <owen.poole@live.com>
2025-11-13 14:21:29 -0800

    updated game.js

--- game.js
+++ game.js
@@ -7,5 +7,16 @@ firstTarget.addEventListener("click", handleClick)
 function handleClick(event) {
   event.currentTarget.remove()
 
-  
+  generateRandomTarget()
+}
+
+function generateRandomTarget() {
+  var target = document.createElement("button")
+
+  target.classList.add("target")
+  target.style.top = getRandomInt(0, 100).toString() + "%"
+  target.style.left = getRandomInt(0, 100).toString() + "%"
+
+  target.addEventListener("click", handleClick)
+  document.getElementsByTagName("body").appendChild(target)
 }
