0500c44fcdbf4381ca632088440bbaf2de6c0388
Owen Poole <owen.poole@live.com>
2025-11-25 19:33:15 -0800

    styled settings modal

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const counterElement = document.getElementById("counter-number")
 const timerElement = document.getElementById("timer-number")
 
 const playAgainButton = document.getElementById("play-again")
-const settingsButton = document.getElementById("settings")
+const openSettingsButton = document.getElementById("open-settings")
 const startRoundButton = document.getElementById("start-round")
 
 const endModalViewScoreboardButton = document.getElementById("view-scoreboard-button")
@@ -12,12 +12,17 @@ const closeScoreboardButton = document.getElementById("close-scoreboard-button")
 const personalButton = document.getElementById("personal-button")
 const globalButton = document.getElementById("global-button")
 
+const gameEndModal = document.getElementById("game-end-modal")
 const saveScoreButton = document.getElementById("save-score-button")
 const saveScoreModal = document.getElementById("save-score-modal")
 const saveScoreModalBackdrop = document.getElementById("save-score-modal-backdrop")
 const cancelSaveScoreButton = document.getElementById("cancel-save-score")
 const userNameInputField = document.getElementById("username-input")
 const submitSaveScoreButton = document.getElementById("submit-save-score")
+
+const settingsModal = document.getElementById("settings-modal")
+const applySettingsButton = document.getElementById("apply-settings-button")
+var settingsOpenedFromGameEndModal;
 
 const gameWindow = document.getElementById("game-window")
 
@@ -46,16 +51,12 @@ endGameButton.addEventListener("click", function() {
   timeLeft = 0;
 })
 
-settingsButton.addEventListener("click", function() {
-  document.getElementById("game-end-modal").classList.add("hidden")
-  document.getElementById("settings-modal").classList.remove("hidden")
-})
-
 gearButton.addEventListener("click", function() {
   if (!gameRunning) {
     scoreboardModal.classList.add("hidden")
     document.getElementById("settings-modal").classList.remove("hidden")
     modalBackdrop.classList.toggle("hidden")
+    settingsOpenedFromGameEndModal = false;
   }
 })
 
@@ -82,6 +83,25 @@ cancelSaveScoreButton.addEventListener("click", hideSaveScoreModal)
 submitSaveScoreButton.addEventListener("click", function() {
   // Send score, username, difficulty, and time to server -> server will append data to a json file
   hideSaveScoreModal()
+})
+
+openSettingsButton.addEventListener("click", function() {
+  settingsOpenedFromGameEndModal = true;
+  gameEndModal.classList.add("hidden")
+  settingsModal.classList.remove("hidden")
+})
+
+applySettingsButton.addEventListener("click", function() {
+  // track difficulty and time selection for use later when adding data for scoreboard
+  
+  if (settingsOpenedFromGameEndModal) {
+    gameEndModal.classList.remove("hidden")
+  }
+  // settings was opened with gear icon on game play screen -> remove modal backdrop
+  else {
+    modalBackdrop.classList.add("hidden")
+  }
+  settingsModal.classList.add("hidden")
 })
 
 endModalViewScoreboardButton.addEventListener("click", function() {
